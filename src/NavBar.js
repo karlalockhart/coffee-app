65b8cef6353c9b5691782b9e4abd5554435bcb91
Karla Lockhart <kblockhart@gmail.com>
2024-02-05 13:47:33 -0500

    updated files

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,7 +5,10 @@ import "./NavBar.css";
 function NavBar() {
   return (
     <nav>
-      <NavLink
+      <NavLink /*this is the link that I'm putting on the page; it's called the nav link; 
+      it's a part of react router and if I put that link on the page, this part tells me that if this thing is clicked, 
+      please go to slass "/", which then hits to "App.js" page > <Route exact path="/" componenet={Home} /> and its like what was that? Where did it go? 
+      It's this one - now render home - this (code below) is what renders the button (when clicked) that causes it to go "Home"*/
         to="/"
         /* add styling to Navlink */
         className="nav-link"
