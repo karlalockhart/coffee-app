1570a9c7c06271b0af088d7c8aa731366f2ca74c
Karla Lockhart <kblockhart@gmail.com>
2024-02-05 14:00:22 -0500

    updated files

--- src/App.js
+++ src/App.js
@@ -13,8 +13,9 @@ const App = () => {
 
   useEffect(() => {
     fetch('http://localhost:3000/coffeeTypes') /* fetch is great because it is asynchronous. Returns a promise object w/ 3 status - pending, error, resolved */
-      .then(res => res.json())
+      .then(res => res.json()) /* as long as that PO shows pending as its status, ill skip the second .then line and the .catch line and keep going to do other stuff, but if it's resolved, it will fire the first & second .then line - it multi-tasks*/
       .then(data => setCoffeeTypes(data))
+      //console.log(data)
       .catch(error => console.error('Error fetching coffee types:', error));
   }, []);
 
